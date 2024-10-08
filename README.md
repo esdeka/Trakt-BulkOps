05805e4e9fe461e4ff3e985796092c5fc145b594
TheFacc <imthefacc@gmail.com>
2024-10-08 19:56:02 +0200

    implement bulk remove by date + misc improvements

--- README.md
+++ README.md
@@ -1,47 +1,86 @@
-Trakt.tv duplicate / additionnal plays bulk removal  
+Trakt BulkOps: bulk remove plays
 ===========  
-This script allows you to remove all the duplicate or additionnal plays for your trakt.tv movies or episodes, and just keep one play per movie / episode (the oldest one).
+This Python script allows users to easily interact with the [Trakt API](https://trakt.tv/b/api-docs) to perform bulk operations on their watch history (movies and episodes).
 
-For instance if you have 3 plays for the movie **Guardians of the Galaxy** at these dates:
+### Operations implemented
+Currently, the operations implemented involve removing plays.
+ - Remove duplicate plays:
+   - Keep the oldest one
+   - Keep one entry per day (remove duplicates within the same day)
+ - Remove all plays from a specific date
+   - Optionally keep some of them
 
- - 2014-08-01T20:00:00.000Z
- - 2017-05-05T22:00:00.000Z
- - 2018-02-05T21:00:00.000Z
 
-The script will keep the oldest play (2014-08-01T20:00:00.000Z) and remove all the others.
-If there are no duplicates the script do nothing.
-The script backup your data in local json files (movies.json and episodes.json) before doing anything.
+### Features
+- Asks the user at startup all the required data, options, and guides throughout the process, no need to edit the script
+- Store Trakt credentials in a dedicated json file
+- Prints plays to be removed before applying any changes and asks for confirmation
+- Allows the user to optionally choose which plays to keep from the list
+
+
+
+### Examples
+
+For instance if you have the following plays in your account:
+
+```
+[0] - 2024-10-02T14:26:50.000Z - JoJo's Bizarre Adventure - S01E24
+[1] - 2024-10-02T14:26:50.000Z - JoJo's Bizarre Adventure - S01E25
+[2] - 2024-10-02T10:31:01.000Z - JoJo's Bizarre Adventure - S01E26
+[3] - 2024-10-02T10:35:26.000Z - JoJo's Bizarre Adventure - S02E01
+[4] - 2024-10-14T10:26:38.000Z - JoJo's Bizarre Adventure - S02E01
+[5] - 2024-10-14T10:26:39.000Z - JoJo's Bizarre Adventure - S02E01
+```
+
+- If running the script to remove duplicates will remove the plays [4] and [5] since they are newer duplicates of [3], the oldest one.
+- If removing duplicates with the option of keeping one per day, the script will only remove the play [5] since it is a duplicate of [4] for the same day.
+- If running the script to remove all plays for the date `2024-10-02`, plays [0],[1],[2],[3] will be removed. In this case, the user is asked the following:
+```
+Enter 'y' to remove all, 'n' to cancel, or specify indexes to skip (e.g., '0 3 12'): 
+```
+Therefore, the user can enter e.g. `3 4` to keep entries [3] and [4] in the account. The _to-remove_ list will be printed again, listing [0],[1],[2],[5], and the user can confirm deletion of the remaining entries.
+
+Based on the operation chosen, if there are no duplicates or there are no entries for the specified date, the script will do nothing.
+
+In any case, the script backups your history in local json files (movies.json and episodes.json) before doing anything.
   
 ## Getting Started  
+### Requirements
+You will need python3 and pip installed and in your path.
+Then install requirements by running the following at the root of the project:
+```
+pip3 install -r requirements.txt
+```
+
+Alternatively, if you are on NixOS or have the Nix package manager, you can simply enter the development shell with python and the required packages by running:
+```
+nix-shell shell.nix
+```
+
+
 ### Trakt.tv  
   
-Go [register a new api app]( https://trakt.tv/oauth/applications/new). And fill the form with theses informations:  
+Go [register a new Trakt.tv API app](https://trakt.tv/oauth/applications/new). And fill the form with this information (other fields are optional):  
   
 ```  
-Name: trakt-duplicates-removal  
+Name: trakt-bulkops
 Redirect uri: urn:ietf:wg:oauth:2.0:oob  
-```  
+```
   
-You don't really care about the others fields.  
-  
-Next, go to [your api applications](https://trakt.tv/oauth/applications) and click on the one named `trakt-duplicates-removal`  
-  
-You will need the `Client ID` and the `Client Secret` from that page.  
-  
+Next, go to [your API applications](https://trakt.tv/oauth/applications) and click on the one named `trakt-bulkops`.
+  You will need the `Client ID` and the `Client Secret` from that page.  
+
 ### The script  
-You will need python3 and pip installed and in your path
- 
- 1. Start by doing a `pip3 install -r requirements.txt` at the root of the project 
- 2. Edit the script to set the `client_id` and `client_secret` variables with the ones you obtained on your trakt.tv api application
- 3. Edit the script to set the `username`variable with your trakt.tv username (you can find it in your settings: https://trakt.tv/settings)
- 4. Optionally you can modify the `types` variable to run the script only for **movies** or **episodes**
- 5. Optionally you can keep one entry per day (per show/movie) by setting the `keep_per_day` variable to `True`
- 6. Launch the script:
+Finally, run the script:
 ```  
-python3 trakt-duplicates-removal.py  
-```  
-Follow the script instructions
+python3 trakt-bulkops.py  
+```
+At the first run, it will ask for your `client_id` and `client_secret` that you obtained on your trakt.tv API application page, and the `username` variable with your Trakt.tv username (you can find it in your settings: https://trakt.tv/settings). These credentials will be stored in the `trakt_credentials.json` file, which can be edited or removed to reset the authentication.
 
-### Credits 
+The script will guide you with the next steps. Depending on your selection, follow the prompts to either delete duplicates or bulk remove plays by date. It asks if you want to apply it to either movies, episodes, or both, etc, and provides feedback on the operation performed.
 
-Based on https://gist.github.com/blaulan/50d462696bce5da9225b8d596a8c6fb4
+## Credits 
+
+Based on https://github.com/anthony-foulfoin/trakt-tv-duplicates-removal which is based on https://gist.github.com/blaulan/50d462696bce5da9225b8d596a8c6fb4
+
+The script currently implements two key features that were of personal interest, but it is designed to be flexible and open to additional functionalities. Feel free to suggest new operations or improvements!
--- trakt-bulkops.py
+++ trakt-bulkops.py
@@ -1,25 +1,48 @@
+import os
 import json
 import requests
 
-# Edit the informations bellow
-client_id = 'YOUR CLIENT ID'
-client_secret = 'YOUR CLIENT SECRET'
-username = 'YOUR USERNAME'
+# File to store the credentials
+CREDENTIALS_FILE = 'trakt_credentials.json'
 
-# Optional
-types = ['movies', 'episodes']  # 'movies' or 'episodes' or both
-keep_per_day = False        # set to True to keep one entry per day
+# Functions
+# credentials
+def save_credentials(client_id, client_secret, username):
+    """Save credentials to a JSON file."""
+    credentials = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'username': username
+    }
+    with open(CREDENTIALS_FILE, 'w') as f:
+        json.dump(credentials, f)
+    print(f"Credentials saved to {CREDENTIALS_FILE}. You can delete or edit this file if needed.\n")
 
+def load_credentials():
+    """Load credentials from the JSON file if it exists."""
+    if os.path.exists(CREDENTIALS_FILE):
+        with open(CREDENTIALS_FILE, 'r') as f:
+            return json.load(f)
+    return None
 
-# Don't edit the informations bellow
-trakt_api = 'https://api.trakt.tv'
-auth_get_token_url = '%s/oauth/token' % trakt_api
-get_history_url = '%s/users/%s/history/{type}?page={page}&limit={limit}' % (trakt_api, username)
-sync_history_url = '%s/sync/history/remove' % trakt_api
+def get_credentials():
+    """Prompt user for credentials if not saved, otherwise load them."""
+    credentials = load_credentials()
 
-session = requests.Session()
+    if credentials:
+        print(f"Loaded credentials from {CREDENTIALS_FILE}")
+        return credentials['client_id'], credentials['client_secret'], credentials['username']
+    else:
+        # Ask user for credentials and save them
+        client_id = input('Enter your Trakt client ID: ')
+        client_secret = input('Enter your Trakt client secret: ')
+        username = input('Enter your Trakt username: ')
 
+        # Save the credentials
+        save_credentials(client_id, client_secret, username)
+        return client_id, client_secret, username
 
+# fetch
 def login_to_trakt():
     print('Authentication')
     print('Open the link in a browser and paste the pin')
@@ -55,7 +78,6 @@ def login_to_trakt():
         'Authorization': 'Bearer ' + response["access_token"]
     })
 
-
 def get_history(type):
     results = []
 
@@ -65,7 +87,7 @@ def get_history(type):
         'type': type
     }
 
-    print('Retrieving history for %s' % type)
+    print('\nRetrieving history for %s' % type)
 
     while True:
         print(get_history_url.format(**url_params))
@@ -86,6 +108,7 @@ def get_history(type):
     return results
 
 
+# act
 def remove_duplicate(history, type):
     print('Removing %s duplicates' % type)
 
@@ -110,14 +133,144 @@ def remove_duplicate(history, type):
         print('No %s duplicates found' % type)
 
 
+def remove_plays_from_specific_date(history, type, date_to_remove):
+    print()
+    print('Searching for %s plays from %s' % (type, date_to_remove))
+
+    entry_type = 'movie' if type == 'movies' else 'episode'
+    plays_to_remove = []
+
+    # Filter entries by date
+    for i in history:
+        watched_at = i['watched_at'].split('T')[0]
+        if watched_at == date_to_remove:
+            plays_to_remove.append(i)
+
+    if len(plays_to_remove) > 0:
+        while True:
+            # Print the plays found to be removed with index
+            print('%s %s plays found:' % (len(plays_to_remove), type))
+            for idx, play in enumerate(plays_to_remove):
+                watched_at = play['watched_at']
+                play_id = play['id']
+
+                if type == 'movies':
+                    # Handle movie-specific details
+                    movie_title = play['movie']['title']
+                    release_year = play['movie'].get('year', 'Unknown Year')
+                    # Print movie format
+                    print(f"[{idx}] {watched_at} - {movie_title} ({release_year})")
+                else:
+                    # Handle episode-specific details
+                    show_name = play['show']['title'] if 'show' in play else ''
+                    season = play[entry_type]['season']
+                    episode = play[entry_type]['number']
+                    episode_title = play[entry_type]['title']
+                    # Print episode format
+                    print(f"[{idx}]\t{watched_at} - {show_name} - S{str(season).zfill(2)}E{str(episode).zfill(2)} - {episode_title}")
+
+            # Ask for confirmation or specify which plays to keep
+            print()
+            print(" - Enter 'y' to remove all plays listed above from your history")
+            print(" - Enter 'n' to cancel and exit")
+            print(" - Enter '0 3 12' (example indexes) to specify what plays you want to keep, and print the list again to check it.")
+            confirm = input("Choose an option: ").lower()
+
+            if confirm == 'y':
+                play_ids = [play['id'] for play in plays_to_remove]
+                session.post(sync_history_url, json={'ids': play_ids})
+                print('%s %s plays successfully removed!' % (len(play_ids), type))
+                break
+            elif confirm == 'n':
+                print('Removal canceled.')
+                break
+            else:
+                try:
+                    # Parse the indexes the user wants to skip
+                    skip_indexes = list(map(int, confirm.split()))
+                    # Filter out the specified indexes from plays_to_remove
+                    plays_to_remove = [play for idx, play in enumerate(plays_to_remove) if idx not in skip_indexes]
+
+                    if len(plays_to_remove) == 0:
+                        print('No plays left to remove. Exiting.')
+                        break
+
+                except ValueError:
+                    print("Invalid input. Please enter 'y', 'n', or a list of indexes (e.g., '0 3 12').")
+    else:
+        print('No %s plays found from %s' % (type, date_to_remove))
+
+
 if __name__ == '__main__':
+    # Get or load credentials
+    client_id, client_secret, username = get_credentials()
+
+    # Don't edit the informations below
+    trakt_api = 'https://api.trakt.tv'
+    auth_get_token_url = '%s/oauth/token' % trakt_api
+    get_history_url = '%s/users/%s/history/{type}?page={page}&limit={limit}' % (trakt_api, username)
+    sync_history_url = '%s/sync/history/remove' % trakt_api
+    session = requests.Session()
+
     login_to_trakt()
 
-    for type in types:
-        history = get_history(type)
-        with open('%s.json' % type, 'w') as output:
-            json.dump(history, output, indent=4)
-            print('History saved in file %s.json' % type)
+    # Ask for purpose
+    print("\nSelect an operation:")
+    print("[1] Bulk remove duplicate plays at any date")
+    print("[2] Bulk remove plays from a specified date")
+    operation = input("Enter the number of the operation (1 or 2): ").strip()
 
-        remove_duplicate(history, type)
-        print()
+    # Ask for target
+    print("\nDo you want to affect:")
+    print("[1] Movies only")
+    print("[2] Episodes only")
+    print("[3] Both movies and episodes")
+    content_type_choice = input("Enter your choice (1, 2, or 3): ").strip()
+
+    # Set types based on user's selection
+    if content_type_choice == '1':
+        types = ['movies']
+    elif content_type_choice == '2':
+        types = ['episodes']
+    elif content_type_choice == '3':
+        types = ['movies', 'episodes']
+    else:
+        print("Invalid choice. Please enter '1', '2', or '3'.")
+        return
+
+
+    # Do
+    if operation == '1':
+        # Remove duplicates operation
+        print("\nYou have selected: Remove duplicate plays")
+        keep_per_day = input("Do you want to keep one entry per day? (y/n): ").strip().lower() == 'y'
+        print("\nRemoving duplicates...\n")
+
+        # Loop through types (movies, episodes)
+        for type in types:
+            print(" ***** MOVIES *****" if type == 'movies' else " ***** EPISODES *****")
+            history = get_history(type)
+            with open(f'{type}.json', 'w') as output:
+                json.dump(history, output, indent=4)
+                print(f'History saved in file {type}.json')
+
+            remove_duplicate(history, type)
+
+    elif operation == '2':
+        # Bulk remove by specific date
+        print("\nYou have selected: Bulk remove plays from a specific date")
+        specific_date = input("Enter the date to remove plays from (YYYY-MM-DD): ").strip()
+        print(f"\nRemoving plays from {specific_date}...\n")
+
+        # Loop through types (movies, episodes)
+        for type in types:
+            print(" ***** MOVIES *****" if type == 'movies' else " ***** EPISODES *****")
+            history = get_history(type)
+            with open(f'{type}.json', 'w') as output:
+                json.dump(history, output, indent=4)
+                print(f'History saved in file {type}.json')
+
+            # Remove plays from the specified date
+            remove_plays_from_specific_date(history, type, specific_date)
+    else:
+        print("Invalid option. Please enter '1' or '2'.")
